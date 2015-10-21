b4307f085982bcc314168c45b4789f0f1124534b
Néstor Bermúdez <nestor.bermudezs@gmail.com>
2015-10-21 07:13:41 -0600

    Initial setup

---
+++ app/src/main/java/com/ericsson/research/owr/sdk/RtcConfigs.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright (c) 2015, Ericsson AB.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+package com.ericsson.research.owr.sdk;
+
+import com.ericsson.research.owr.HelperServerType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class RtcConfigs {
+    private static final String TAG = "RtcConfigs";
+
+    private RtcConfigs() {
+    }
+
+    public static RtcConfig defaultConfig(String stunServerUrl) {
+        return new Default(stunServerUrl);
+    }
+
+    public static RtcConfig defaultConfig(Collection<RtcConfig.HelperServer> helperServers) {
+        return new Default(helperServers);
+    }
+
+    private static class Default extends RtcConfig {
+        private static final List<RtcPayload> sDefaultVideoPayloads = new ArrayList<>(3);
+        static {
+ /*           sDefaultVideoPayloads.add(new RtcPayloadImpl(103, "H264", 90000, new HashMap<String, Object>(){{
+                put("packetization-mode", 1);
+            }}, 0, false, true, true)); */
+//            FIXME: Enable when Chrome can handle an offer with RTX for H264
+/*            sDefaultVideoPayloads.add(new PlainRtcPayload(123, "RTX", 90000, new HashMap<String, Object>(){{
+                put("apt", 103);
+                put("rtx-time", 200);
+            }}, 0, false, false, false));*/
+            sDefaultVideoPayloads.add(new RtcPayloadImpl(100, "VP8", 90000, null, 0, true, true, true));
+            sDefaultVideoPayloads.add(new RtcPayloadImpl(120, "RTX", 90000, new HashMap<String, Object>(){{
+                put("apt", 100);
+                put("rtx-time", 200);
+            }}, 0, false, false, false));
+        }
+
+        private static final List<RtcPayload> sDefaultAudioPayloads = new ArrayList<>(3);
+        static {
+            sDefaultAudioPayloads.add(new RtcPayloadImpl(111, "OPUS", 48000, null, 2, false, false, false));
+            sDefaultAudioPayloads.add(new RtcPayloadImpl(8, "PCMA", 8000, null, 1, false, false, false));
+            sDefaultAudioPayloads.add(new RtcPayloadImpl(0, "PCMU", 8000, null, 1, false, false, false));
+        }
+
+        private final Collection<HelperServer> mHelperServers;
+
+        private Default(Collection<HelperServer> helperServers) {
+            mHelperServers = helperServers;
+        }
+
+        private Default(String stunServerUrl) {
+            String[] split = stunServerUrl.split(":");
+            if (split.length < 1 || split.length > 2) {
+                throw new IllegalArgumentException("invalid stun server url: " + stunServerUrl);
+            }
+            final int port;
+            if (split.length == 2) {
+                port = Integer.parseInt(split[1]);
+            } else {
+                port = 3478;
+            }
+            mHelperServers = Arrays.asList(new HelperServer(HelperServerType.STUN, split[0], port, "", ""));
+        }
+
+        @Override
+        protected List<RtcPayload> getDefaultVideoPayloads() {
+            return sDefaultVideoPayloads;
+        }
+
+        @Override
+        protected List<RtcPayload> getDefaultAudioPayloads() {
+            return sDefaultAudioPayloads;
+        }
+
+        @Override
+        protected boolean shouldRespectRemotePayloadOrder() {
+            return false;
+        }
+
+        @Override
+        protected Collection<HelperServer> getHelperServers() {
+            return mHelperServers;
+        }
+    }
+}
---
+++ app/src/main/java/com/ericsson/research/owr/sdk/SimpleStreamSet.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2015, Ericsson AB.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or other
+ * materials provided with the distribution.
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+ * OF SUCH DAMAGE.
+ */
+package com.ericsson.research.owr.sdk;
+
+import android.util.Log;
+
+import com.ericsson.research.owr.AudioRenderer;
+import com.ericsson.research.owr.MediaSource;
+import com.ericsson.research.owr.MediaType;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SimpleStreamSet extends StreamSet {
+    private static final String TAG = "SimpleStreamSet";
+
+    private final boolean mWantVideo;
+    private final boolean mWantAudio;
+
+    private  AudioRenderer mAudioRenderer;
+
+    private  SimpleMediaStream mAudioStream;
+    private final SimpleMediaStream mVideoStream;
+
+    private VideoSourceProvider mRemoteVideoSourceProvider = new VideoSourceProvider();
+
+    private SimpleStreamSet(MediaSourceProvider audioSourceProvider, MediaSourceProvider videoSourceProvider,
+                            boolean sendAudio, boolean sendVideo) {
+        mWantAudio = sendAudio;
+        mWantVideo = sendVideo;
+
+        mVideoStream = new SimpleMediaStream(true);
+        mVideoStream.setStreamMode(StreamMode.SEND_ONLY);
+
+        videoSourceProvider.addMediaSourceListener(new MediaSourceListener() {
+            @Override
+            public void setMediaSource(final MediaSource mediaSource) {
+                mVideoStream.setMediaSource(mediaSource);
+            }
+        });
+       
+        // sujeeth - disable audio.
+        // set video to send only.
+        
+        if (mWantAudio){
+        	 mAudioRenderer = new AudioRenderer();
+             mAudioStream = new SimpleMediaStream(false);
+        	mAudioStream.setStreamMode(StreamMode.SEND_RECEIVE);
+        	
+        	 audioSourceProvider.addMediaSourceListener(new MediaSourceListener() {
+                 @Override
+                 public void setMediaSource(final MediaSource mediaSource) {
+                     mAudioStream.setMediaSource(mediaSource);
+                 }
+             });
+        	 
+        }
+        
+       
+
+       
+    }
+
+    private class VideoSourceProvider implements MediaSourceProvider {
+        private MediaSourceListenerSet set = new MediaSourceListenerSet();
+
+        public void notifyListeners(MediaSource mediaSource) {
+            set.notifyListeners(mediaSource);
+        }
+
+        @Override
+        public void addMediaSourceListener(final MediaSourceListener listener) {
+            set.addListener(listener);
+        }
+    }
+
+    public VideoView createRemoteView() {
+        return new VideoViewImpl(mRemoteVideoSourceProvider, 0, 0, 0);
+    }
+
+    /**
+     * Creates a configuration for setting up a basic audio/video call.
+     *
+     * @param sendAudio true if audio should be sent, audio may still be received
+     * @param sendVideo true if video should be sent, video may still be received
+     * @return a new RtcConfig with a simple audio/video call configuration
+     */
+    public static SimpleStreamSet defaultConfig(boolean sendAudio, boolean sendVideo) {
+        return new SimpleStreamSet(MicrophoneSource.getInstance(), CameraSource.getInstance(), sendAudio, sendVideo);
+    }
+
+    @Override
+    protected List<? extends Stream> getStreams() {
+    	return Arrays.asList(mVideoStream);
+        //return Arrays.asList(mAudioStream, mVideoStream);
+    }
+
+    /**
+     * @return the current audio renderer pipeline graph in dot format.
+     */
+    public String dumpPipelineGraph() {
+        return mAudioRenderer.getDotData();
+    }
+
+    private class SimpleMediaStream extends MediaStream {
+        private final String mId;
+        private final boolean mIsVideo;
+        private MediaSource mMediaSource;
+        private MediaSourceDelegate mMediaSourceDelegate;
+
+        private SimpleMediaStream(boolean isVideo) {
+            mIsVideo = isVideo;
+            mId = Utils.randomString(27);
+        }
+
+        @Override
+        protected String getId() {
+            return mId;
+        }
+
+        @Override
+        protected MediaType getMediaType() {
+            return mIsVideo ? MediaType.VIDEO : MediaType.AUDIO;
+        }
+
+        @Override
+        protected boolean wantSend() {
+            return mIsVideo ? mWantVideo : mWantAudio;
+        }
+
+        @Override
+        protected boolean wantReceive() {
+        	if (mIsVideo)
+        		return false;
+        	else
+        		return true;
+        }
+
+        @Override
+        protected void onRemoteMediaSource(final MediaSource mediaSource) {
+            if (mIsVideo) {
+                mRemoteVideoSourceProvider.notifyListeners(mediaSource);
+            } else {
+                mAudioRenderer.setSource(mediaSource);
+            }
+        }
+
+        @Override
+        protected synchronized void setMediaSourceDelegate(final MediaSourceDelegate mediaSourceDelegate) {
+            mMediaSourceDelegate = mediaSourceDelegate;
+            if (mMediaSource != null && mediaSourceDelegate != null) {
+                mediaSourceDelegate.setMediaSource(mMediaSource);
+            }
+        }
+
+        @Override
+        public void setStreamMode(final StreamMode mode) {
+            Log.i(TAG, (mIsVideo ? "video" : "audio") + " stream mode set: " + mode.name());
+        }
+
+        public synchronized void setMediaSource(final MediaSource mediaSource) {
+            mMediaSource = mediaSource;
+            if (mMediaSourceDelegate != null) {
+                mMediaSourceDelegate.setMediaSource(mediaSource);
+            }
+        }
+    }
+}
